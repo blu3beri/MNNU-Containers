a0dfcfa72b4065869df3705495f7891887c86f66
blu3beri <dev@blueberi.nl>
2020-10-22 16:16:03 +0200

    fixed issues with local binding

--- start-dev.sh
+++ start-dev.sh
@@ -1,7 +1,7 @@
 aca-py start \
     -it acapy_plugin_toolbox.http_ws 0.0.0.0 3000 \
     -ot http \
-    -e http://localhost:3000 ws://localhost:3001 \
+    -e http://localhost:3000 localhost:3000/http/ws \
     --label "Test Agent" \
     --auto-accept-requests \
     --auto-ping-connection \
@@ -17,9 +17,8 @@ aca-py start \
     --invite-role admin \
     --invite-label "Test Agent (admin)" \
     --genesis-url http://dev.greenlight.bcovrin.vonx.io/genesis \
-    --wallet-type indy \
     --plugin acapy_plugin_toolbox \
-    --admin 0.0.0.0 3000 \
+    --admin 0.0.0.0 3002 \
     --admin-insecure-mode \
     --debug-connections \
     --debug-credentials \
