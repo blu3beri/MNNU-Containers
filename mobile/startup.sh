3250397f0b3c9e2754bc5f19caef8462a76ea4ca
blu3beri <dev@blueberi.nl>
2020-11-19 11:57:00 +0100

    removed schemas

--- mobile/startup.sh
+++ mobile/startup.sh
@@ -2,7 +2,7 @@ aca-py start \
     -it acapy_plugin_toolbox.http_ws 0.0.0.0 "$PORT" \
     -ot http \
     -ot ws \
-    -e "$ENDPOINT" "${ENDPOINT/http/ws}" \
+    -e "$ENDPOINT" "${ENDPOINT}/http/ws" \
     --label "$AGENT_NAME" \
     --auto-accept-requests --auto-ping-connection \
     --auto-respond-credential-proposal --auto-respond-credential-offer --auto-respond-credential-request --auto-store-credential \
